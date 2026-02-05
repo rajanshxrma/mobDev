320d06442cbea1a6ae1ba1bd6dd0e336a6c27aa9
Rajan Sharma <rajansharma@Rajans-MacBook-Pro.local>
2026-02-04 20:03:15 -0500

    Add main.dart - Flutter tabs demo app

---
+++ main.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: _TabsNonScrollableDemo(),
+      ),
+    );
+  }
+}
+
+class _TabsNonScrollableDemo extends StatefulWidget {
+  @override
+  _TabsNonScrollableDemoState createState() => _TabsNonScrollableDemoState();
+}
+
+class _TabsNonScrollableDemoState extends State<_TabsNonScrollableDemo>
+    with SingleTickerProviderStateMixin, RestorationMixin {
+  late TabController _tabController;
+
+  final RestorableInt tabIndex = RestorableInt(0);
+
+  @override
+  String get restorationId => 'tab_non_scrollable_demo';
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(tabIndex, 'tab_index');
+    _tabController.index = tabIndex.value;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 4,
+      vsync: this,
+    );
+    _tabController.addListener(() {
+      setState(() {
+        tabIndex.value = _tabController.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    tabIndex.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tabs = ['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4'];
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Tabs Demo',
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [
+            for (final tab in tabs) Tab(text: tab),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          // Tab 1 - Text Widget with AlertDialog
+          Container(
+            color: Colors.red[100],
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome to Tab 1',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Alert'),
+                            content: Text('This is an alert dialog!'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Text('Show Alert'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Tab 2 - Image Widget with Input text widgets
+          Container(
+            color: Colors.green[100],
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your email',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Image.network(
+                    'https://example.com/image.jpg',
+                    width: 150,
+                    height: 150,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Tab 3 - Button Widget with SnackBar
+          Container(
+            color: Colors.blue[100],
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Button pressed in ${tabs[2]} tab!'),
+                    ),
+                  );
+                },
+                child: Text('Click me'),
+              ),
+            ),
+          ),
+          // Tab 4 - ListView Widget with Card widgets
+          Container(
+            color: Colors.purple[100],
+            child: ListView(
+              children: [
+                Card(
+                  child: ListTile(title: Text('Item 1')),
+                ),
+                Card(
+                  child: ListTile(title: Text('Item 2')),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      // Bottom App Bar
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
